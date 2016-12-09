4cc1eab671bcba920cdae002fd06381bca767f02
Zhuhao Wang <zhuhaow@gmail.com>
2016-12-09 15:16:37 +0800

    Add support for custom queue

--- Resolver/Resolver.swift
+++ Resolver/Resolver.swift
@@ -8,7 +8,7 @@ public enum ResolveType: DNSServiceProtocol {
 }
 
 public class Resolver {
-    fileprivate static let queue = DispatchQueue(label: "ResolverQueue")
+    public static var queue = DispatchQueue(label: "ResolverQueue")
     
     public static var activeCount: Int {
         return dict.count
