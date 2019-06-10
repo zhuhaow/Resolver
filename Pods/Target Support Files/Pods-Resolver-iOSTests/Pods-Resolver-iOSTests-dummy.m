f98f4589a1d0c2a01afc789f593a496fce6c0c4a
Zac Gorak <zac@trackoff.com>
2019-06-10 16:06:44 -0400

    Added Pods folder

---
+++ Pods/Target Support Files/Pods-Resolver-iOSTests/Pods-Resolver-iOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Resolver_iOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_Resolver_iOSTests
+@end
---
+++ Pods/Target Support Files/Pods-Resolver-macOS/Pods-Resolver-macOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Resolver_macOS : NSObject
+@end
+@implementation PodsDummy_Pods_Resolver_macOS
+@end
